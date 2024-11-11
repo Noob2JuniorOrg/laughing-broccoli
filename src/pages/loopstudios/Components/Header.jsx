adf9d2bc526e2f84abe7a34650c7f1785e0d355a
Nikolay Pashov <nikolaypashov9@gmail.com>
2024-11-11 12:38:41 +0200

    Added a functioning responsive menu for the mobile version

--- src/pages/loopstudios/Components/Header.jsx
+++ src/pages/loopstudios/Components/Header.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../images/logo.svg';
 import hamburger from '../images/icon-hamburger.svg';
+import MobileMenu from './MobileMenu';
 
 const Header = () => {
+  const [menu_opened, setMenuOpened] = useState(false);
+  const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+  const toggleMenu = () => {
+    if (!isMenuClicked) {
+      setMenuOpened(true);
+    } else {
+      setMenuOpened(false);
+    }
+    setIsMenuClicked(!isMenuClicked);
+  };
+
   return (
-    <div className="relative pt-[2.667em] px-[1.6em] pb-0 xl:p-0 h-[43.333rem] bg-[url('./pages/loopstudios/images/mobile/image-hero.jpg')] xl:bg-[url('./pages/loopstudios/images/desktop/image-hero.jpg')] bg-no-repeat bg-cover bg-black/40">
+    <div className="relative pt-[2.667em] px-[1.6em] pb-0 xl:p-0 h-[40.625rem] xl:h-[43.333rem] bg-[url('./pages/loopstudios/images/mobile/image-hero.jpg')] xl:bg-[url('./pages/loopstudios/images/desktop/image-hero.jpg')] bg-no-repeat bg-cover bg-black/40">
       <div className="absolute inset-0 bg-black opacity-0 xl:opacity-40 h-full"></div>
       <div className="flex justify-between relative xl:px-[165px] xl:pt-[64px]">
         <div className="flex justify-between w-full xl:w-[192px]">
@@ -15,11 +28,11 @@ const Header = () => {
               className="w-[9rem] h-[1.5rem] xl:w-[12rem] xl:h-[2rem]"
             />
           </a>
-          <div className="xl:hidden">
+          <div className="relative xl:hidden" onClick={toggleMenu}>
             <img src={hamburger} alt="" />
           </div>
         </div>
-        <nav className="hidden font-loopstudios text-white xl:flex justify-between xl:min-w-[380px] ">
+        <nav className="relative hidden font-loopstudios text-white xl:flex justify-between xl:min-w-[380px] ">
           <a href="#" className="relative underline-middle">
             About
           </a>
@@ -37,7 +50,8 @@ const Header = () => {
           </a>
         </nav>
       </div>
-      <div className="relative xl:w-[40.625rem] xl:h-[17.375rem] border-white border-2 p-[2.5rem] mt-[163px] xl:mt-[123px] xl:ml-[165px]">
+      <div>{menu_opened && <MobileMenu />}</div>
+      <div className="relative xl:w-[40.625rem] xl:h-[17.375rem] border-white border-2 p-[1.5rem] xl:p-[2.5rem] mt-[163px] xl:mt-[123px] xl:ml-[165px]">
         <p className="font-looptstudios-headline font-light uppercase text-white text-[2.5em] xl:text-[4.5em] leading-[2.375rem] xl:leading-[4.375rem] h-full">
           Immersive experiences that deliver
         </p>
---
+++ src/pages/loopstudios/Components/MobileMenu.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import logo from '../images/logo.svg';
+import closeIcon from '../images/icon-close.svg';
+import { useState } from 'react';
+
+const MobileMenu = () => {
+  const [isMenuOpened, setIsMenuOpened] = useState(true);
+  const [isCloseIconClicked, setCloseIsIconClicked] = useState(false);
+
+  const closeMenu = () => {
+    if (!isCloseIconClicked) {
+      setIsMenuOpened(true);
+    } else {
+      setIsMenuOpened(false);
+    }
+    setCloseIsIconClicked(!isCloseIconClicked);
+  };
+
+  return (
+    <div
+      className={`xl:hidden  left-0 top-0 h-[667px] bg-black w-full z-50 px-[1.5rem] pt-[2.5rem] ${isMenuOpened ? 'absolute' : 'hidden'}`}
+    >
+      <div className="flex justify-between items-center ">
+        <a href="#home">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-[9rem] h-[1.5rem] xl:w-[12rem] xl:h-[2rem]"
+          />
+        </a>
+        <div onClick={closeMenu}>
+          <img src={closeIcon} alt="icon-close" />
+        </div>
+      </div>
+      <div className="flex flex-col justify-between font-looptstudios-headline font-extralight text-[1.5em] text-white uppercase h-[221px] mt-[159px]">
+        <a href="#">About</a>
+        <a href="#">Careers</a>
+        <a href="#">Events</a>
+        <a href="#">Products</a>
+        <a href="#">Support</a>
+      </div>
+    </div>
+  );
+};
+
+export default MobileMenu;
