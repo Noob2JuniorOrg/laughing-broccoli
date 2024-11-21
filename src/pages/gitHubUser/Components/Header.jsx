7b8e459c757a885c205e5556144a115712b21bcf
Nikolay Pashov <nikolaypashov9@gmail.com>
2024-11-21 21:04:21 +0200

    Created the search bar component

--- src/pages/gitHubUser/Components/Header.jsx
+++ src/pages/gitHubUser/Components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import moon from '../Assets/icon-moon.svg';
+import sun from '../Assets/icon-sun.svg';
 import { useDarkMode } from '../DarkModeContext';
 
 const Header = () => {
@@ -7,15 +8,28 @@ const Header = () => {
 
   return (
     <div className="flex justify-between items-center font-github-user font-bold text-black">
-      <h2 className="text-[26px]">devfinder</h2>
+      <h2 className="text-[26px] dark:text-white">devfinder</h2>
       <div
         className=" flex gap-4 cursor-pointer items-center group"
         onClick={toggleDarkMode}
       >
-        <p className=" uppercase tracking-[2.5px] text-[13px] text-light-grey group-hover:text-[black]">
+        <p className="uppercase tracking-[2.5px] text-[13px] text-light-grey dark:hidden group-hover:text-[black]">
           Dark
         </p>
-        <img src={moon} alt="them-icon" className="group-hover:fill-black" />
+        <p className="uppercase tracking-[2.5px] text-[13px] text-white hidden dark:block group-hover:text-[#90A4D4]">
+          Light
+        </p>
+        <img
+          src={moon}
+          alt="theme-icon"
+          className="group-hover:fill-black dark:hidden"
+        />
+
+        <img
+          src={sun}
+          alt="theme-icon"
+          className="hidden dark:block group-hover:fill-[#90A4D4]"
+        />
       </div>
     </div>
   );
---
+++ src/pages/gitHubUser/Components/SearchBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import searchIcon from '../Assets/icon-search.svg';
+
+const SearchBar = () => {
+  return (
+    <div className="flex w-full h-[3.75rem] mt-[2.25rem] md:h-[4.313rem] bg-white dark:bg-dark-blue rounded-[15px] pl-4 pr-[7px] md:pl-8 md:pr-[0.625rem] items-center justify-between font-github-user">
+      <div className="flex gap-[8.95px] md:gap-6 group">
+        <img src={searchIcon} alt="searchIcon" />
+        <input
+          type="text"
+          className="focus:outline-none min-w-[184px] md:min-w-[254px] dark:bg-dark-blue text-navy-blue dark:text-white placeholder:text-navy-blue placeholder:text-[13px] placeholder:leading-[25px] md:placeholder:text-[18px] placeholder:overflow-ellipsis dark:placeholder:text-[white] text-[13px] md:text-[1rem] xl:text-[18px] leading-[25px]"
+          placeholder="Search GitHub username"
+        />
+      </div>
+      <div className="group">
+        <button className="w-[5.25rem] h-[2.875rem] md:w-[106px] md:h-[50px] bg-light-blue text-white text-[14px] md:text-[1rem] font-bold rounded-[10px] group-hover:bg-[#60ABFF]">
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
