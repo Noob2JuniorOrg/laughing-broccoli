184ef9a2ac0f6c360cf3323a07cc1bb2289fbf22
Nikolay Pashov <nikolaypashov9@gmail.com>
2025-04-22 04:30:13 +0300

    Extracted mobile mode image selectors in separate components

--- src/pages/eCommerce/Components/ProductGallery.jsx
+++ src/pages/eCommerce/Components/ProductGallery.jsx
@@ -2,6 +2,8 @@ import { thumbnails } from '../Data/data';
 import Thumbnails from './Thumbnails';
 import { useGallery } from '../Contexts/GalleryContext';
 import '../Styling/productGallery.css';
+import PrevImgMobile from './IconComponents/PrevImgMobile';
+import NextImgMobile from './IconComponents/NextImgMobile';
 
 const ProductGallery = () => {
   const { selectedIndex, toggleGallery, prevItem, nextItem } = useGallery();
@@ -9,18 +11,7 @@ const ProductGallery = () => {
   return (
     <div className="product-gallery-container">
       <div className="product-gallery-sizing ">
-        {/* icon-prev */}
-        <span className="prev-img-mobile" onClick={prevItem}>
-          <svg width="12" height="18" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M11 1 3 9l8 8"
-              stroke="#1D2026"
-              strokeWidth="3"
-              fill="none"
-              fillRule="evenodd"
-            ></path>
-          </svg>
-        </span>
+        <PrevImgMobile prevItem={prevItem} />
         {/* Image on focus */}
         <img
           src={thumbnails[selectedIndex]?.displayImage}
@@ -29,21 +20,7 @@ const ProductGallery = () => {
           index={selectedIndex}
           onClick={toggleGallery}
         />
-        {/* icon-next */}
-        <span
-          className="next-img-mobile"
-          onClick={() => nextItem(thumbnails.length - 1)}
-        >
-          <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="m2 1 8 8-8 8"
-              stroke="#1D2026"
-              strokeWidth="3"
-              fill="none"
-              fillRule="evenodd"
-            ></path>
-          </svg>
-        </span>
+        <NextImgMobile nextItem={nextItem} />
       </div>
       <Thumbnails indexType="main" />
     </div>
