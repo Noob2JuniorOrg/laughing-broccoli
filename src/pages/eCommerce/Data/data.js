508f37c2fd18729644abc52b09d2ae0f44f97584
Nikolay Pashov <nikolaypashov9@gmail.com>
2025-05-05 14:27:01 +0300

    Changed import style of thumbnails in data.js in eCommerce Store

--- src/pages/eCommerce/Data/data.js
+++ src/pages/eCommerce/Data/data.js
@@ -1,3 +1,13 @@
+import thumb1 from 'src/pages/eCommerce/images/image-product-1-thumbnail.jpg';
+import thumb2 from 'src/pages/eCommerce/images/image-product-2-thumbnail.jpg';
+import thumb3 from 'src/pages/eCommerce/images/image-product-3-thumbnail.jpg';
+import thumb4 from 'src/pages/eCommerce/images/image-product-4-thumbnail.jpg';
+
+import full1 from 'src/pages/eCommerce/images/image-product-1.jpg';
+import full2 from 'src/pages/eCommerce/images/image-product-2.jpg';
+import full3 from 'src/pages/eCommerce/images/image-product-3.jpg';
+import full4 from 'src/pages/eCommerce/images/image-product-4.jpg';
+
 export const nav_links_mobile = [
     { target: 'Collection', key: '1' },
     { target: 'Men', key: '2' },
@@ -15,9 +25,8 @@ export const nav_links_desktop = [
 ]
 
 export const thumbnails = [
-    { src: 'src/pages/eCommerce/images/image-product-1-thumbnail.jpg', displayImage: 'src/pages/eCommerce/images/image-product-1.jpg' },
-    { src: 'src/pages/eCommerce/images/image-product-2-thumbnail.jpg', displayImage: 'src/pages/eCommerce/images/image-product-2.jpg' },
-    { src: 'src/pages/eCommerce/images/image-product-3-thumbnail.jpg', displayImage: 'src/pages/eCommerce/images/image-product-3.jpg' },
-    { src: 'src/pages/eCommerce/images/image-product-4-thumbnail.jpg', displayImage: 'src/pages/eCommerce/images/image-product-4.jpg' },
-
-]
+    { src: thumb1, displayImage: full1 },
+    { src: thumb2, displayImage: full2 },
+    { src: thumb3, displayImage: full3 },
+    { src: thumb4, displayImage: full4 },
+];
